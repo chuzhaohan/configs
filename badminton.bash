b88b45ef56ba38d32e7e06a0e0f6b2a47f4bbd28
deepak <dpak.cherian@gmail.com>
2014-04-10 17:24:02 -0400

    better matplotlibrc location

--- badminton.bash
+++ badminton.bash
@@ -1,10 +1,11 @@
 rm recschedule*
 
 # download schedule
-wget http://web.mit.edu/athletics/www/recschedule.pdf
+wget http://web.mit.edu/athletics/www/recschedule.pdf -O ~/recschedule.pdf
 
 # convert to text
 pdftotext -layout ~/recschedule.pdf ~/recschedule.txt
 
 # extract Badminton records with dates and remove others
-cat recschedule.txt | grep -B 1 'day\|Badminton' | grep -v "Start\|Volleyball\|Basketball"
+# then print all lines after current day
+cat ~/recschedule.txt | grep -B 1 'day\|Badminton' | grep -v "Start\|Volleyball\|Basketball" | grep -A 500 $(date +%A)
