391c2e98b265fa0c3b9a52eb21366ccf29829b2e
deepak <dpak.cherian@gmail.com>
2014-04-16 11:53:43 -0400

    updated queuecheck so tat it doesn't trip up on characters in other fields

--- queuecheck.bash
+++ queuecheck.bash
@@ -7,14 +7,22 @@ export SGE_CELL=default
 export SGE_ARCH=lx-amd64
 
 # cron requires absolute path because environment isn't set
-QSTAT=/share/apps/gridengine/62u5_20110621/bin/lx-amd64/qstat
-GREP=/bin/grep
-AWK=/usr/bin/awk
+qstat=/share/apps/gridengine/62u5_20110621/bin/lx-amd64/qstat
+grep=/bin/grep
+awk=/usr/bin/awk
+cut=/usr/bin/cut
 
 # parse qstat output. Use GREP instead of -u because that results in a title
 # line passing checks. I do not want that
-OUT=$($QSTAT | $GREP "dcherian" | $GREP "\bR\|\bS\|\bt" | $AWK '{print $3'})
+#status=$($QSTAT | $AWK '{print $1, $5}')
+# get job numbers that satisfy criterion
+#job=$( echo $status | $GREP "\br\|\bS\|\bt\|\bR" | $AWK '{print $1}' )
 
+#job=$($QSTAT | $AWK '{print $1, $5}' | $GREP "\br" | $AWK '{print $1}')
+job=$( echo $($qstat | $awk '{print $1, $5}' | $grep "\bt\|\bS\|\bR" | $awk '{print $1}') | $cut -d ' ' -f3-)
+# get job name for above job numbers
+OUT=$($qstat | $grep "dcherian" | $grep ${job// /\\\|} | $awk '{print $3'})
+echo $OUT
 # check that cron is working
 #DATE=$(date)
 #echo "running on cron: $DATE" >> ~/croncheck
