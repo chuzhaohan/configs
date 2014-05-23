6378ef8207567bffecfe3debd8ebdfebe86f3319
deepak <dpak.cherian@gmail.com>
2014-05-22 23:04:31 -0400

    removed elpa files

--- .emacs.d/elpa/easy-kill-20140422.837/easy-kill-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "easy-kill" "20140422.837" "kill & mark things easily"
-  '((emacs "24")
-    (cl-lib "0.5"))
-  :url "https://github.com/leoliu/easy-kill" :keywords
-  '("killing" "convenience"))
--- .emacs.d/elpa/f-20140220.21/f-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; f-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("f-pkg.el" "f.el") (21334 47616 573351
-;;;;;;  139000))
-
-;;;***
-
-(provide 'f-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; f-autoloads.el ends here
