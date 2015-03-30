f889d59065c1d22687e2d03bde7f28fa8fb4bf08
deepak <dpak.cherian@gmail.com>
2015-03-30 15:28:09 -0400

    updates to dc-latex.el

--- .emacs.d/personal/dc-latex.el
+++ .emacs.d/personal/dc-latex.el
@@ -1,7 +1,8 @@
-;;; Commentary
+;;; Commentary:
 ;; This contains my auctex/latex configuration
 ;; - Deepak Cherian
 
+;;; Code:
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
 (setq-default TeX-master nil)
@@ -11,6 +12,8 @@
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
 
+(setq TeX-source-correlate-method 'synctex)
+
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 (setq reftex-plug-into-AUCTeX t)
 (setq TeX-PDF-mode t)
@@ -19,3 +22,20 @@
       (quote
        (("^pdf$" "." "evince -f %o")
         ("^html?$" "." "iceweasel %o"))))
+
+;; Following from:
+;; https://stackoverflow.com/questions/3627574/emacs-changing-the-default-reftex-citation
+(defvar reftex-last-citation nil)
+
+(defadvice reftex-citation (after reftex-citation-and-remember-citation activate)
+  "Save last citation to 'reftex-last-citation after running 'reftex-citation"
+  (setq reftex-last-citation ad-return-value))
+
+(defadvice reftex-get-bibkey-default (around reftex-just-return-last-citation activate)
+  "If there is a 'reftex-last-citation then just return that instead of running 'reftex-get-bibkey-default"
+  (if reftex-last-citation
+      (setq ad-return-value reftex-last-citation)
+    ad-do-it))
+
+(provide 'dc-latex)
+;;; dc-latex.el ends here
