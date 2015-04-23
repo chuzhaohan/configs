b8404ca8983a5ed32edc42b2bd8201ab5356c97c
deepak <dpak.cherian@gmail.com>
2015-04-23 16:36:40 -0400

    cleanup org-mode export. Now I can have a fmt line in each org file and it works!
    There doesn't seem to be a way to alternate between pdflatex and xelatex - unless I set
    org-latex-pdf-process to call some external script that decides which to use.

--- .emacs.d/personal/dc-org.el
+++ .emacs.d/personal/dc-org.el
@@ -24,25 +24,25 @@
 
 ;; Originally taken from Bruno Tavernier: http://thread.gmane.org/gmane.emacs.orgmode/31150/focus=31432
 ;; but adapted to use latexmk 4.20 or higher.
-(defun my-auto-tex-cmd ()
-  "When exporting from .org with latex, automatically run latex,
-     pdflatex, or xelatex as appropriate, using latexmk."
-  (let ((texcmd)))
-  ;; default command: oldstyle latex via dvi
-  (setq texcmd "latexmk -dvi -pdfps -quiet %f")
-  ;; pdflatex -> .pdf
-  (if (string-match "LATEX_CMD: pdflatex" (buffer-string))
-      (setq texcmd "latexmk -pdf  -f %f"))
-  ;; xelatex -> .pdf
-  (if (string-match "LATEX_CMD: xelatex" (buffer-string))
-      (setq texcmd "latexmk -pdflatex=\"xelatex -fmt=/media/data/Work/tools/latex/preamble.fmt\" -pdf %f"))
-  ;; LaTeX compilation command
-  (setq org-latex-pdf-process (list texcmd)))
+;; (defun my-auto-tex-cmd ()
+;;   "When exporting from .org with latex, automatically run latex,
+;;      pdflatex, or xelatex as appropriate, using latexmk."
+;;   (let ((texcmd)))
+;;   ;; default command: oldstyle latex via dvi
+;;   (setq texcmd "latexmk -dvi -pdfps -quiet %f")
+;;   ;; pdflatex -> .pdf
+;;   (if (string-match "LATEX_CMD: pdflatex" (buffer-string))
+;;       (setq texcmd "latexmk -pdf  -f %f"))
+;;   ;; xelatex -> .pdf
+;;   (if (string-match "LATEX_CMD: xelatex" (buffer-string))
+;;       (setq texcmd "latexmk -pdflatex=\"xelatex -fmt=/media/data/Work/tools/latex/preamble.fmt\" -pdf %f"))
+;;   ;; LaTeX compilation command
+;;   (setq org-latex-pdf-process '(list texcmd)))
 
 ;;(setq texcmd "latexmk -pdflatex=\"xelatex -fmt=/media/data/Work/tools/latex/preamble.fmt\" -pdf %f")
 ;;(setq org-latex-to-pdf-process (list texcmd))
 
-;;(remove-hook 'org-export-latex-after-initial-vars-hook 'my-auto-tex-cmd)
+;;(add-hook 'org-export-latex-before-parsing-hook 'my-auto-tex-cmd)
 (setq org-latex-create-formula-image-program 'dvipng)
 
 (setq org-file-apps
@@ -50,115 +50,61 @@
        ("\\.pdf\\'" . "evince %s")))
 
 ;; org-latex-pdf-process is for org > 8.0
+;; remove blank lines - so that I can use format statement in
+;; #+LATEX_HEADER. set jobname so that it opens the pdf. %b command
+;; found in ox-latex.el
 (setq org-latex-pdf-process
-      '("PATH=/usr/local/texlive/2014/bin/x86_64-linux/:$PATH latexmk -pdflatex='xelatex -fmt=/media/data/Work/tools/latex/preamble.fmt' -gg -pdf %f"))
+      '("tail -n +3 %f > %f.nolines; PATH=/usr/local/texlive/2014/bin/x86_64-linux/:$PATH latexmk -pdflatex=xelatex -jobname=%b -gg -pdf %f.nolines"))
+;;(setq org-latex-pdf-process
+;;      '("PATH=/usr/local/texlive/2014/bin/x86_64-linux/:$PATH latexmk -pdflatex=xelatex ;;-gg -pdf %f")
 
 (defun my-auto-tex-parameters ()
-      "Automatically select the tex packages to include."
-      ;; default packages for ordinary latex or pdflatex export
-      (setq org-latex-default-packages-alist
-            '((""     "inputenx" t)
-              ("T1"   "fontenc"   t)
-              ("sc"   "mathpazo"  t)
-              (""     "fixltx2e"  nil)
-              (""     "wrapfig"   nil)
-              (""     "soul"      t)
-              (""     "xfrac"     t)
-              (""     "bigints"   t)
-              (""     "textcomp"  t)
-              (""     "marvosym"  t)
-              (""     "wasysym"   t)
-              (""     "latexsym"  t)
-              (""     "mathtools" t)
-              (""     "amssymb"   t)
-              ("tight-spacing=true"     "siunitx"   nil)
-              (""     "cleveref"  nil)
-              (""     "graphicx"  nil)
-              (""     "natbib"    nil)
-              (""     "hyperref"  nil)))
+  "Automatically select the tex packages to include."
+  ;; default packages for ordinary latex or pdflatex export
+  (setq org-latex-default-packages-alist
+        '((""     "inputenx" t)
+          ("T1"   "fontenc"   t)
+          ("sc"   "mathpazo"  t)
+          (""     "fixltx2e"  nil)
+          (""     "wrapfig"   nil)
+          (""     "soul"      t)
+          (""     "xfrac"     t)
+          (""     "bigints"   t)
+          (""     "textcomp"  t)
+          (""     "marvosym"  t)
+          (""     "wasysym"   t)
+          (""     "latexsym"  t)
+          (""     "mathtools" t)
+          (""     "amssymb"   t)
+          ("tight-spacing=true"     "siunitx"   nil)
+          (""     "cleveref"  nil)
+          (""     "graphicx"  nil)
+          (""     "natbib"    nil)
+          (""     "hyperref"  nil)))
 
-      ;; Packages to include when xelatex is used
-      (if (string-match "LATEX_CMD: xelatex" (buffer-string))
-          (setq org-latex-default-packages-alist
-                '(("" "fontspec" t)
-                  ("" "unicode-math" t)))))
+  ;; Packages to include when xelatex is used
+  (if (string-match "LATEX_CMD: xelatex" (buffer-string))
+      (setq org-latex-default-packages-alist
+            '(("" "fontspec" t)
+              ("" "unicode-math" t)))))
 
 ;;(add-hook 'org-export-preprocess-before-selecting-backend-code-hook 'my-auto-tex-parameters)
 
 ;; my customized preamble
 (add-to-list 'org-latex-classes
              '("dcarticle"
-                "[NO-DEFAULT-PACKAGES]"
+               "[NO-DEFAULT-PACKAGES]"
                 ("\\section{%s}" . "\\section*{%s}")
                 ("\\subsection{%s}" . "\\subsection*{%s}")
                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-;;            '("dc-article"
-;;               "\\documentclass[11pt,oneside]{memoir}\n
-;;\\usepackage{color}
-;;\\usepackage{fixltx2e}
-;;\\usepackage{graphicx}   % add figures
-;;\\usepackage{xcolor} 	 % support for color names
-;;\\usepackage{array}
-;;\\usepackage{booktabs}    % fancy tables
-;;\\usepackage{multirow}
-;;\\usepackage{longtable}
-;;\\usepackage{subfig}	% add subfigure support
-;;\\usepackage{float}	     % stronger placement of floats
-;;\\usepackage{ulem}
-;;\\usepackage{etoolbox}
-;;\\usepackage{letltxmacro}  % for new sqrt
-;;\\usepackage{parskip}
-;;\\usepackage{titlesec}
-;;\\usepackage{titletoc}
-;;\\usepackage{paralist}
-;;\\usepackage{rotating} % allow page rotation
-;;\\usepackage{pdflscape} % allow landscape pages in pdf
-;;\\usepackage{hyperref}  % hyperlinks in pdf
-;;\\usepackage[figure,table]{hypcap} % Correct a problem with hyperref
-;;\\usepackage{mathtools}
-;;\\usepackage{bigints}
-;;\\usepackage[warning]{onlyamsmath}
-;;\\usepackage[sort&compress]{natbib} % for bibliography
-;;\\usepackage{cleveref}
-;;\\usepackage{gensymb}
-;;\\usepackage{xfrac}
-;;\\usepackage{siunitx}
-;;\\usepackage[protrusion=true]{microtype}
-;;\\linespread{1.5}
-;;\\urlstyle{rm}
-;;\\newcommand{\\ir}{\\mathrm{i}}
-;;\\newcommand{\\dr}{\\mathrm{d}}
-;;\\newcommand{\\D}{\\mathrm{D}}
-;;\\newcommand{\\e}{\\, \\mathrm{e}}
-;;\\newcommand{\\er}{\\mathrm{e}}
-;;\\newcommand{\\dd}[3][]{{\\frac{\\dr^{#1} #2}{\\dr #3^{#1}}}}
-;;\\newcommand{\\pp}[3][]{{\\frac{\\partial^{#1} #2}{\\partial #3^{#1}}}}
-;;\\newcommand{\\DD}[1]{{\\frac{\\D#1}{\\D t}}}
-;;\\newcommand{\\mb}[1]{\\boldsymbol{#1}} % math bold font
-;;\\newcommand{\\ol}[1]{\\overline{#1}}
-;;\\newcommand{\\x}{\\times}
-;;\\newcommand{\\mO}{\\mathcal{O}}
-;;\\newcommand{\\ubr}{\\underbracket}
-;;\\newcommand{\\disp}{\\displaystyle}
-;;\\newcommand{\\vb}{\\mb{v}\\xspace}
-;;\\newcommand{\\ub}{\\mb{u}\\xspace}
-;;\\newcommand{\\Ro}{\\mathit{Ro}}
-;;\\newcommand{\\Rot}{\\mathit{Ro}_\mathit{T}}
-;;\\newcommand{\\Rh}{\\mathit{Rh}}
-;;\\newcommand{\\Ri}{\\mathit{Ri}}
-;;\\newcommand{\\Rif}{\\Ri_f}
-;;\\newcommand{\\Pra}{\\mathit{Pr}}
-;;\\newcommand{\\Pe}{\\mathit{Pe}}
-;;\\newcommand{\\Ra}{\\mathit{Ra}}
-;;\\newcommand{\\Rey}{\\mathit{Re}}
-;;\\newcommand{\\Fr}{\\mathit{Fr}}
-;;\\newcommand{\\Bu}{\\mathit{Bu}}"
-;;               ("\\section{%s}" . "\\section*{%s}")
-;;               ("\\subsection{%s}" . "\\subsection*{%s}")
-;;               ("\\paragraph{%s}" . "\\paragraph*{%s}")
-;;               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-;;
+
+(add-to-list 'org-latex-classes
+             '("dcbeamer"
+               "[NO-DEFAULT-PACKAGES]"
+                ("\\section{%s}" . "\\section*{%s}")
+                ("\\subsection{%s}" . "\\subsection*{%s}")
+                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
 (provide 'dc-org)
 ;;; dc-org ends here
