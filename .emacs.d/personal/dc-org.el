5550706055040ef43e1af2ed61b9c950bddb67c1
deepak <dpak.cherian@gmail.com>
2016-04-12 11:46:19 -0400

    remove unused org-mode code

--- .emacs.d/personal/dc-org.el
+++ .emacs.d/personal/dc-org.el
@@ -6,22 +6,15 @@
 ;; org-mode class for my latex style
 (require 'ox-latex)
 
-(setq org-latex-listings t)
-(setq org-startup-indented t)
-
-(setq org-ellipsis "⤵")
-
-;; org mode todo faces
-;;(setq org-todo-keyword-faces
-;;      '(("TODO" . org-warning) ("STARTED" . "yellow")
-;;        ("CANCELED" . (:foreground "blue" :weight bold))
-;;        ("DONE" . "green")))
-
 ;; org mode keybindings
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+(setq org-latex-listings t)
+(setq org-startup-indented t)
+(setq org-ellipsis "⤵")
 (setq org-log-done t)
 (setq org-catch-invisible-edits 'show)
 (setq org-list-allow-alphabetical t)
@@ -47,28 +40,6 @@
          "\\1*" data nil nil 1)
       data)))
 (setq org-export-filter-headline-functions '(headline-numbering-filter))
-
-;; Originally taken from Bruno Tavernier: http://thread.gmane.org/gmane.emacs.orgmode/31150/focus=31432
-;; but adapted to use latexmk 4.20 or higher.
-;; (defun my-auto-tex-cmd ()
-;;   "When exporting from .org with latex, automatically run latex,
-;;      pdflatex, or xelatex as appropriate, using latexmk."
-;;   (let ((texcmd)))
-;;   ;; default command: oldstyle latex via dvi
-;;   (setq texcmd "latexmk -dvi -pdfps -quiet %f")
-;;   ;; pdflatex -> .pdf
-;;   (if (string-match "LATEX_CMD: pdflatex" (buffer-string))
-;;       (setq texcmd "latexmk -pdf  -f %f"))
-;;   ;; xelatex -> .pdf
-;;   (if (string-match "LATEX_CMD: xelatex" (buffer-string))
-;;       (setq texcmd "latexmk -pdflatex=\"xelatex -fmt=/media/data/Work/tools/latex/preamble.fmt\" -pdf %f"))
-;;   ;; LaTeX compilation command
-;;   (setq org-latex-pdf-process '(list texcmd)))
-
-;;(setq texcmd "latexmk -pdflatex=\"xelatex -fmt=/media/data/Work/tools/latex/preamble.fmt\" -pdf %f")
-;;(setq org-latex-to-pdf-process (list texcmd))
-
-;;(add-hook 'org-export-latex-before-parsing-hook 'my-auto-tex-cmd)
 (setq org-latex-create-formula-image-program 'dvipng)
 
 (setq org-file-apps
@@ -81,41 +52,6 @@
 ;; found in ox-latex.el
 (setq org-latex-pdf-process
       '("export BSTINPUTS=/usr/local/texlive/2014/texmf-dist/bibtex/bst/elsarticle/; tail -n +2 %f | sed '/./,$!d' > %f.nolines; mv %f.nolines %f; latexmk -gg %f; exiftool -overwrite_original -Producer=`git log -n 1 --pretty=%H` %b.pdf"))
-
-;;(setq org-latex-pdf-process
-;;'("PATH=/usr/local/texlive/2014/bin/x86_64-linux/:$PATH latexmk -pdflatex=xelatex ;;-jobname=%b -gg -pdf %f; exiftool -overwrite_original -Producer=`git log -n 1 ;;--pretty=%H` %b.pdf"))
-
-(defun my-auto-tex-parameters ()
-  "Automatically select the tex packages to include."
-  ;; default packages for ordinary latex or pdflatex export
-  (setq org-latex-default-packages-alist
-        '((""     "inputenx" t)
-          ("T1"   "fontenc"   t)
-          ("sc"   "mathpazo"  t)
-          (""     "fixltx2e"  nil)
-          (""     "wrapfig"   nil)
-          (""     "soul"      t)
-          (""     "xfrac"     t)
-          (""     "bigints"   t)
-          (""     "textcomp"  t)
-          (""     "marvosym"  t)
-          (""     "wasysym"   t)
-          (""     "latexsym"  t)
-          (""     "mathtools" t)
-          (""     "amssymb"   t)
-          ("tight-spacing=true"     "siunitx"   nil)
-          (""     "cleveref"  nil)
-          (""     "graphicx"  nil)
-          (""     "natbib"    nil)
-          (""     "hyperref"  nil)))
-
-  ;; Packages to include when xelatex is used
-  (if (string-match "LATEX_CMD: xelatex" (buffer-string))
-      (setq org-latex-default-packages-alist
-            '(("" "fontspec" t)
-              ("" "unicode-math" t)))))
-
-;;(add-hook 'org-export-preprocess-before-selecting-backend-code-hook 'my-auto-tex-parameters)
 
 ;; my customized preamble
 (add-to-list 'org-latex-classes
