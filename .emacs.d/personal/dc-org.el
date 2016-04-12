421f495c03f3de7c22345c28822869d74c79e2ca
deepak <dpak.cherian@gmail.com>
2016-04-12 11:45:45 -0400

    some org-mode tweaks. looking awesomer

--- .emacs.d/personal/dc-org.el
+++ .emacs.d/personal/dc-org.el
@@ -28,6 +28,13 @@
 (setq org-export-in-background nil)
 (setq org-latex-prefer-user-labels t)
 (setq org-latex-tables-booktabs t)
+(setq org-hide-emphasis-markers t)
+(font-lock-add-keywords 'org-mode
+                        '(("^ +\\([-*]\\) "
+                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(require 'org-bullets)
+(add-hook 'org-mode-hook 'org-bullets-mode)
 
 (defun headline-numbering-filter (data backend info)
   "No numbering in headlines that have a property :numbers: no"
